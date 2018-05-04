973848b28792aab1d8a1c3c1444bda138e27387f
Dima <some@mail>
2018-05-04 16:13:29 +0400

    creating the owin startup class and eidited web.config

---
+++ SportsStoreWebApi/App_Start/IdentityConfig.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly:OwinStartup(typeof(SportsStoreWebApi.IdentityConfig))]
+
+namespace SportsStoreWebApi
+{
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app) { }
+    }
+}
