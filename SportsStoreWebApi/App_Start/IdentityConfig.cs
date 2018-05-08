6a4e19a083f87378dc3b7ca17cc51811582300e3
Dima <some@mail>
2018-05-09 00:03:02 +0400

    Adding the configuration statements

--- SportsStoreWebApi/App_Start/IdentityConfig.cs
+++ SportsStoreWebApi/App_Start/IdentityConfig.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security.Cookies;
+using SportsStoreWebApi.Infrastructure.Identity;
 
 [assembly:OwinStartup(typeof(SportsStoreWebApi.IdentityConfig))]
 
@@ -7,6 +10,16 @@ namespace SportsStoreWebApi
 {
     public class IdentityConfig
     {
-        public void Configuration(IAppBuilder app) { }
+        public void Configuration(IAppBuilder app) {
+            app.CreatePerOwinContext<StoreIdentityDbContext>(StoreIdentityDbContext.Create);
+
+            app.CreatePerOwinContext<StoreUserManager>(StoreUserManager.Create);
+            app.CreatePerOwinContext<StoreRoleManager>(StoreRoleManager.Create);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie
+            });
+        }
     }
 }
