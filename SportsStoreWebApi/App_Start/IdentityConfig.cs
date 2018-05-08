66df2267ce4c1f1e1b16c70695a46bb1f70b003e
Dima <some@mail>
2018-05-09 01:25:20 +0400

    Configuring Authentication

--- SportsStoreWebApi/App_Start/IdentityConfig.cs
+++ SportsStoreWebApi/App_Start/IdentityConfig.cs
@@ -3,6 +3,8 @@ using Owin;
 using Microsoft.AspNet.Identity;
 using Microsoft.Owin.Security.Cookies;
 using SportsStoreWebApi.Infrastructure.Identity;
+using System;
+using Microsoft.Owin.Security.OAuth;
 
 [assembly:OwinStartup(typeof(SportsStoreWebApi.IdentityConfig))]
 
@@ -14,6 +16,13 @@ namespace SportsStoreWebApi
             app.CreatePerOwinContext<StoreIdentityDbContext>(StoreIdentityDbContext.Create);
             app.CreatePerOwinContext<StoreUserManager>(StoreUserManager.Create);
             app.CreatePerOwinContext<StoreRoleManager>(StoreRoleManager.Create);
+
+            app.UseOAuthBearerTokens(new OAuthAuthorizationServerOptions
+            {
+                Provider = new StoreAuthProvider(),
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/Authenticate")
+            });
         }
     }
 }
